b0eddc028b774bfd49a5723a323b01aac90222d1
Marbett Hedderich <marbett@gmail.com>
2021-06-02 13:34:53 +0200

    peticion http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 //provide in root me evita tener que importar en los providers, lo eleva al nivel del root
@@ -6,12 +7,17 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
-  constructor() { }
-
   private _history: string [] = [];
+  private _apiKey: string = 'fGN7RRKQBvzVIUTSR0KGAG07HKeKIau5';
+  private _limit: number = 10;
 
   get history () {
       return [...this._history];
+  }
+
+  constructor(private httpClient: HttpClient) { 
+
+
   }
 
   searchGifs (query: string = '') {
@@ -22,6 +28,33 @@ export class GifsService {
       this._history = this._history.splice(0, 10);
     }
     console.log(this._history);
+    
+    //esta peticion es observable
+    this.httpClient.get(`https://api.giphy.com/v1/gifs/search?api_key=${this._apiKey}&q=${query}&limit=${this._limit}`)
+    .subscribe(
+      (resp: any) => {
+        console.log(resp.data);
+      }
+    );
   }
+
+
+  /*
+  async searchGifs (query: string = '') {
+    query = query.toLowerCase();
+    
+    if (!this._history.includes(query)) {
+      this._history.unshift(query);
+      this._history = this._history.splice(0, 10);
+    }
+    console.log(this._history);
+
+    const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=fGN7RRKQBvzVIUTSR0KGAG07HKeKIau5&q=gato&limit=10');
+    const data = await resp.json();
+    console.log(data);
+
+
+  }
+  */
 
 }
